38217cf2b43b5b1ee09a6e4c76ec3ab236fdc2e7
tkohlhauff <tkohlhauff@gmail.com>
2017-03-21 16:38:27 -0500

    Cleaned and Condensed

--- Starship.py
+++ Starship.py
@@ -26,15 +26,9 @@ x_change = 0
 y_change = 0
 
 lastDir = 3 # 0 = left, 1 = up, 2 = right, 3 = down
-# Sprite animation steps, 3 per direction; Top is for 1,2,3,2,1 cycle; detect if 3 or 1
-leftAn = 0
-leftTop = False
-rightAn = 0
-rightTop = False
-upAn = 0
-upTop = False
-downAn = 0
-downTop = False
+# Sprite animation step count once it reaches three set topCount to true and go down
+topCount = False
+an = 0
 
 playerImg = pygame.image.load('Wizard-front-2.png')
 
@@ -49,132 +43,105 @@ def player(img,x,y):
     gameDisplay.blit(playerImg,(x,y))
 
 def animation():
-    global lastDir, downTop, upTop, rightTop, leftTop, downAn, upAn, rightAn, leftAn
+    global lastDir, topCount, an
     global x, y, x_change, y_change, sprites,pressed
     #Checks direction from keypressed and moves count for sprite animation
-    if lastDir == 0:
-        if leftTop:
-            leftAn -= 1
-        else:
-            leftAn += 1
-
-    if lastDir == 2:
-        if rightTop:
-            rightAn -= 1
-        else:
-            rightAn += 1
-        
-    if lastDir == 1:
-        if upTop:
-            upAn -= 1
-        else:
-            upAn += 1
-
-    if lastDir == 3:
-        if downTop:
-            downAn -= 1
-        else:
-            downAn += 1
-    if rightAn == 2:
-        rightTop = True
-    elif rightAn == 0:
-        rightTop = False
-
-    if leftAn == 2:
-        leftTop = True
-    elif leftAn == 0:
-        leftTop = False
-
-    if upAn == 2:
-        upTop = True
-    elif upAn == 0:
-        upTop = False
-
-    if downAn == 2:
-        downTop = True
-    elif downAn == 0:
-        downTop = False
-#### Took from line 237
+    if topCount:
+        an -= 1
+    else:
+        an += 1
+    if an==2:
+        topCount=True
+    elif an==0:
+        topCount=False
+    # Movement for the player
     if x + x_change >= 0 and x + x_change <= display_width - player_width:
             x += x_change
     if y + y_change >= 0 and y + y_change <= display_height - player_height:
         y += y_change
-    #check for key up during animation
+
+    #check for key change during animation as well as let go of key
     for event in pygame.event.get():
+        if event.type==pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                pressed=True
+                lastDir=0
+            if event.key == pygame.K_UP:
+                pressed=True
+                lastDir=1
+            if event.key == pygame.K_RIGHT:
+                pressed=True
+                lastDir=2
+            if event.key == pygame.K_DOWN:
+                pressed=True
+                lastDir = 3
         if event.type==pygame.KEYUP:
             pressed=False
     #Draws the sprites based on direction and change in x,y position      
     gameDisplay.fill(white)
     if lastDir == 0:
-        player(sprites[2][leftAn],x,y)
+        player(sprites[2][an],x,y)
         
     elif lastDir == 1:
-        player(sprites[0][upAn],x,y)
+        player(sprites[0][an],x,y)
         
     elif lastDir == 2:
-        player(sprites[3][rightAn],x,y)
+        player(sprites[3][an],x,y)
 
     elif lastDir == 3:
-        player(sprites[1][downAn],x,y)
+        player(sprites[1][an],x,y)
     
     pygame.display.update()
     clock.tick(30)
 
 def game_loop():
-    global lastDir, leftAn, rightAn, upAn, downAn, rightTop, leftTop, upTop, downTop
-    global x, y, x_change, y_change, pressed
+    global lastDir, x_change, y_change, pressed
 
     # Game close boolean
     gameExit = False
-    stepCount = 0
 
     while not gameExit:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 gameExit = True
             
-
-            keys = pygame.key.get_pressed()
+        #Checks the button press and determines the direction, 0 left, 1 up, 2 right, 3 down
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    pressed=True
-                if event.key == pygame.K_UP:
-                    pressed=True
-                if event.key == pygame.K_RIGHT:
-                    pressed=True
-                if event.key == pygame.K_DOWN:
-                    pressed=True
-            while keys[pygame.K_LEFT] and pressed==True:
-                x_change = -5
-                lastDir = 0
-                animation()
-                x_change=0                
-            while keys[pygame.K_UP]and pressed==True:
-                y_change = -5
-                lastDir = 1
-                animation()
-                y_change=0 
-
-            while keys[pygame.K_RIGHT]and pressed==True:
-                x_change = 5
-                lastDir = 2
-                animation()
-                x_change=0 
-
-            while keys[pygame.K_DOWN]and pressed==True:
-                y_change = 5
-                lastDir = 3
-                animation()
-                y_change=0 
-                        
                 if event.key == pygame.K_ESCAPE:
                     gameExit = True
+                if event.key == pygame.K_LEFT:
+                    pressed=True
+                    lastDir=0
+                if event.key == pygame.K_UP:
+                    pressed=True
+                    lastDir=1
+                if event.key == pygame.K_RIGHT:
+                    pressed=True
+                    lastDir=2
+                if event.key == pygame.K_DOWN:
+                    pressed=True
+                    lastDir = 3
+        #use a loop for button pressses, needed for long presses
+            while pressed==True:
+                if lastDir == 0:
+                    x_change = -5
+                    animation()
+                    x_change=0
+                if lastDir == 1:
+                    y_change = -5
+                    animation()
+                    y_change=0 
 
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                    x_change = 0
-                if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                    y_change = 0
+                if lastDir == 2:
+                    x_change = 5
+                    animation()
+                    x_change=0 
+
+                if lastDir == 3:
+                    y_change = 5
+                    animation()
+                    y_change=0 
+                        
 
         
 
